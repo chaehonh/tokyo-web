3fbef9ec55fd355f8fe1ba212d7847c657a53629
chaehonh <minchaehong@gmail.com>
2025-12-28 21:41:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,32 +4,50 @@ function showDay(day) {
   if (day === 1) {
     content.innerHTML = `
       <img src="day1.jpg" class="photo">
-      <h2>Day 1</h2>
-      <ul>
-        <li>시부야 스카이</li>
-        <li>이치란 라멘</li>
-        <li>시부야 거리 산책</li>
-      </ul>
+      <div class="card-content">
+        <h2>Day 1 · Shibuya</h2>
+        <p>
+          도쿄 첫날 ✈️  
+          시부야에서 시작한 여행.
+        </p>
+        <ul>
+          <li>#시부야</li>
+          <li>#도쿄여행</li>
+          <li>#tokyo</li>
+        </ul>
+      </div>
     `;
   } else if (day === 2) {
     content.innerHTML = `
       <img src="day2.jpg" class="photo">
-      <h2>Day 2</h2>
-      <ul>
-        <li>디즈니씨</li>
-        <li>돈카츠 마루시치</li>
-        <li>기념품 쇼핑</li>
-      </ul>
+      <div class="card-content">
+        <h2>Day 2 · DisneySea</h2>
+        <p>
+          하루 종일 있어도 부족한 디즈니씨 🎢  
+          분위기 최고.
+        </p>
+        <ul>
+          <li>#디즈니씨</li>
+          <li>#여행스타그램</li>
+          <li>#disneysea</li>
+        </ul>
+      </div>
     `;
   } else {
     content.innerHTML = `
       <img src="day3.jpg" class="photo">
-      <h2>Day 3</h2>
-      <ul>
-        <li>츠지한</li>
-        <li>백화점 방문</li>
-        <li>공항 이동</li>
-      </ul>
+      <div class="card-content">
+        <h2>Day 3 · Goodbye Tokyo</h2>
+        <p>
+          마지막 날 🥹  
+          아쉬움 가득 안고 귀국.
+        </p>
+        <ul>
+          <li>#도쿄맛집</li>
+          <li>#여행끝</li>
+          <li>#goodbye</li>
+        </ul>
+      </div>
     `;
   }
 }
