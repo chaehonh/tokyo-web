e749e2fc9ba974027793392a3fedb86caaaa737c
chaehong <minchaehong@gmail.com>
2025-12-30 19:32:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function showDay(day) {
       <div class="card-content">
         <h2>Day 1 시부야 🌸</h2>
         <p class="meta">📅 2025.07 · 📍 Tokyo</p>
-        <p>도쿄 도착! ✈️ 시부야에서 하루를 시작했다.</p>
+        <p>도쿄 도착! ✈️  시부야 한 바퀴 돌고 라멘으로 마무리 🍜</p>
 
         <div class="photo-wrap">
           <img src="day1.jpg" class="photo" alt="Day 1 사진 1">
@@ -26,9 +26,9 @@ function showDay(day) {
 
         <div class="divider"></div>
         <ul>
-          <li>시부야</li>
-          <li>라멘</li>
-          <li>첫날</li>
+          <li>>🛍️ 시부야</li>
+          <li>>🍜 라멘</li>
+          <li>>📸 첫날</li>
         </ul>
       </div>
     `;
@@ -37,7 +37,7 @@ function showDay(day) {
       <div class="card-content">
         <h2>Day 2 디즈니씨 🎠</h2>
         <p class="meta">📅 2025.07 · 📍 Tokyo</p>
-        <p>하루 종일 디즈니씨 💫 진짜 꿈나라 같았다.</p>
+        <p>하루 종일 디즈니씨 💫 디즈니랜드도 가보고싶어졌당 </p>
 
         <div class="photo-wrap">
           <img src="day2.jpg" class="photo" alt="Day 2 사진">
@@ -45,9 +45,9 @@ function showDay(day) {
 
         <div class="divider"></div>
         <ul>
-          <li>디즈니씨</li>
-          <li>놀이기구</li>
-          <li>야경최고</li>
+          <li>🎡디즈니씨</li>
+          <li>🍿놀이기구</li>
+          <li>🌙야경최고</li>
         </ul>
       </div>
     `;
@@ -56,7 +56,8 @@ function showDay(day) {
       <div class="card-content">
         <h2>Day 3 귀국 🧸</h2>
         <p class="meta">📅 2025.07 · 📍 Tokyo</p>
-        <p>마지막 날 🥹 쇼핑 후 아쉬운 마음으로 귀국.</p>
+        <p>🧾 마지막 날은 쇼핑 위주!<br>
+      캐리어 무게 늘리고 공항으로 🛫</p>
 
         <div class="photo-wrap">
           <img src="day3.jpg" class="photo" alt="Day 3 사진 1">
@@ -65,9 +66,9 @@ function showDay(day) {
 
         <div class="divider"></div>
         <ul>
-          <li>맛집</li>
-          <li>쇼핑</li>
-          <li>굿바이</li>
+          <li>🍱맛집</li>
+          <li>🛒쇼핑</li>
+          <li>✈️굿바이</li>
         </ul>
       </div>
     `;
@@ -77,3 +78,4 @@ function showDay(day) {
 document.addEventListener("DOMContentLoaded", () => {
   showDay(1);
 });
+
